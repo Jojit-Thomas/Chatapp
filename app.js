9e2d33862edf4ac30be8434cdcedf4f33df0b69a
Jojit Thomas <thomasjojit@gmail.com>
2022-05-27 09:40:16 +0530

    added process.env.port

--- app.js
+++ app.js
@@ -35,8 +35,10 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on :3000');
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log('Server listening on :' + port);
 });
 
 
